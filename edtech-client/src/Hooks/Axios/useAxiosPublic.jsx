e6e43c2520e376a4633e1783fe67a417a15570bb
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-09-11 08:05:52 +0600

    backend issue fix

--- edtech-client/src/Hooks/Axios/useAxiosPublic.jsx
+++ edtech-client/src/Hooks/Axios/useAxiosPublic.jsx
@@ -4,8 +4,8 @@ const axiosPublic = axios.create({
   // baseURL: "https://edtech-react.vercel.app/api",
   // baseURL: `http://localhost:4000/api`,
   // baseURL: `${import.meta.env.VITE_url}`,
-  // baseURL: import.meta.env.VITE_url,
-  baseURL: import.meta.env.VITE_url_production,
+  baseURL: import.meta.env.VITE_url,
+  // baseURL: import.meta.env.VITE_url_production,
 });
 
 export default function useAxiosPublic() {
